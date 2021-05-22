3c71a424eae7a833838bb79abd86c1f176441ed2
linsaftw <25271111+linsaftw@users.noreply.github.com>
2021-05-22 15:30:40 -0300

    Removed particle variable

--- src/main/java/dev/_2lstudios/hypercosmetics/players/CosmeticPlayer.java
+++ src/main/java/dev/_2lstudios/hypercosmetics/players/CosmeticPlayer.java
@@ -1,12 +1,9 @@
 package dev._2lstudios.hypercosmetics.players;
 
-import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 
 public class CosmeticPlayer {
-
 	private final Player bukkitPlayer;
-	private Particle arrowTrail;
 
 	CosmeticPlayer(final Player player) {
 		this.bukkitPlayer = player;
@@ -15,13 +12,4 @@ public class CosmeticPlayer {
 	public Player getBukkitPlayer() {
 		return this.bukkitPlayer;
 	}
-
-	public Particle getArrowTrail() {
-		return this.arrowTrail;
-	}
-
-	public void setArrowTrail(Particle arrowTrail) {
-		this.arrowTrail = arrowTrail;
-	}
-
 }
