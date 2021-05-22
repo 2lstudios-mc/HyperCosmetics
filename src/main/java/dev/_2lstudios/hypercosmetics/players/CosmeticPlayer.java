5bd9a97cf7210fee7b0c98d189bd8cbf3bcc0b0e
linsaftw <25271111+linsaftw@users.noreply.github.com>
2021-05-22 15:31:24 -0300

    Added killeffect variable

--- src/main/java/dev/_2lstudios/hypercosmetics/players/CosmeticPlayer.java
+++ src/main/java/dev/_2lstudios/hypercosmetics/players/CosmeticPlayer.java
@@ -2,8 +2,11 @@ package dev._2lstudios.hypercosmetics.players;
 
 import org.bukkit.entity.Player;
 
+import dev._2lstudios.hypercosmetics.cosmetics.killeffects.KillEffect;
+
 public class CosmeticPlayer {
 	private final Player bukkitPlayer;
+	private KillEffect killEffect;
 
 	CosmeticPlayer(final Player player) {
 		this.bukkitPlayer = player;
@@ -11,5 +14,13 @@ public class CosmeticPlayer {
 
 	public Player getBukkitPlayer() {
 		return this.bukkitPlayer;
+	}
+
+	public KillEffect getKillEffect() {
+		return killEffect;
+	}
+
+	public void setKillEffect(KillEffect killEffect) {
+		this.killEffect = killEffect;
 	}
 }
